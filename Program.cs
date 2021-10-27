e2237fd9df59f44d1bf8857353a96c4e6559dd3f
Gary-learner <gary1900106644@gmail.com>
2021-10-27 10:38:38 +0800

    Initiated CSharp console project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace XiangQiGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to our XiangQiGame! We are Team 3! ");
+        }
+    }
+}
