f8e77a86dc5b13a62a77c9c949b2f62d804f6578
Gary-learner <gary1900106644@gmail.com>
2021-10-27 11:04:39 +0800

    Next

--- Program.cs
+++ Program.cs
@@ -7,6 +7,25 @@ namespace XiangQiGame
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to our XiangQiGame! We are Team 3! ");
+
+            // Start writting some basic classes:
+            //    - Board
+            //    - ChessPieces
+            //       - Solider
+            //       - General
+            //       - etc...
+
+            // ChessPieces
+            //   --> Method return the name of the piece
+            //   --> piece = new Soldier()
+            //       console.WriteLine("My name is " & piece.name()) --> "My Name is Soldier"
+
+            // Basic game routine
+            //   Player 1 and player 2
+            //   Each turn, ask the player to choose a piece, and move
+
+            // Print the board ??
+            
         }
     }
 }
