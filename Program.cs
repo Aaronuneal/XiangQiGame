6fafa53c50d6fe803c4ecbe7eda54f4678d97e93
Gary-learner <gary1900106644@gmail.com>
2021-11-06 10:31:33 +0800

    push

--- Program.cs
+++ Program.cs
@@ -2,11 +2,88 @@
 
 namespace XiangQiGame
 {
-    class Program
+    //class Program
+    //{
+        public abstract class ChessPieces
+  {
+      public enum ChessTypes
+      {Soldier,Cannon,Chariot,Hourse,Elephant,Advisor,General}
+    // member variables
+    
+    String Name;
+
+    public ChessPieces(string Name)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Welcome to our XiangQiGame! We are Team 3! ");
+      this.Name = Name;
+    }
+
+ 
+  public class Soldier : ChessPieces // Declaration is incomplete
+  {
+    public Soldier(string Name)
+      : base(Name)
+      {
+          Name = "Soldier";
+      }
+  }
+  
+  public class Cannon : ChessPieces // Declaration is incomplete
+  {
+    public Cannon(string Name)
+      : base(Name)
+      {
+          Name = "Cannon";
+      }
+  }
+
+  public class Chariot : ChessPieces // Declaration is incomplete
+  {
+    public Chariot(string Name)
+      : base(Name)
+      {
+          Name = "Chariot";
+      }
+  }
+
+  public class Horse : ChessPieces // Declaration is incomplete
+  {
+    public Horse(string Name)
+      : base(Name)
+      {
+          Name = "Horse";
+      }
+  }
+  
+  public class Elephant : ChessPieces // Declaration is incomplete
+  {
+    public Elephant(string Name)
+      : base(Name)
+      {
+          Name = "Elephant";
+      }
+  }
+
+  public class Advisor : ChessPieces // Declaration is incomplete
+  {
+    public Advisor(string Name)
+      : base(Name)
+      {
+          Name = "Advisor";
+      }
+  }
+
+  public class General : ChessPieces // Declaration is incomplete
+  {
+    public General(string Name)
+      : base(Name)
+      {
+          Name = "General";
+      }
+  }
+
+}
+}
+
 
             // Start writting some basic classes:
             //    - Board
@@ -25,7 +102,3 @@ namespace XiangQiGame
             //   Each turn, ask the player to choose a piece, and move
 
             // Print the board ??
-            
-        }
-    }
-}
